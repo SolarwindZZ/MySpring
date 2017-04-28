d2c316ece74879015e5927237868704c37b34a82
solar <zheng.zhang25@139.com>
2017-04-28 23:33:22 +0800

    Initial push to github

--- src/com/zz/hello/HelloController.java
+++ src/com/zz/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created by zz on 16/10/12.
+ * Created by zz on 2017/4/28.
  */
 @Controller
 @RequestMapping("/")
@@ -16,6 +16,7 @@ public class HelloController {
     public String printHello(ModelMap model) {
         model.addAttribute("msg", "Spring MVC Hello World");
         model.addAttribute("name", "Zhang Zheng");
+
         return "hello";
     }
 
